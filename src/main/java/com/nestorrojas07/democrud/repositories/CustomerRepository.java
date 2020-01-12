b86b5cba5577bb460259fdfcef1af53ecc4cdbd5
Nestor Rojas <nestor.rojas@zinobe.com>
2020-01-12 18:17:53 -0500

    Automatic Query

--- src/main/java/com/nestorrojas07/democrud/repositories/CustomerRepository.java
+++ src/main/java/com/nestorrojas07/democrud/repositories/CustomerRepository.java
@@ -3,7 +3,10 @@
  */
 package com.nestorrojas07.democrud.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nestorrojas07.democrud.models.Customer;
 
@@ -11,6 +14,27 @@ import com.nestorrojas07.democrud.models.Customer;
  * @author nrojas
  *
  */
+@Repository
 public interface CustomerRepository  extends JpaRepository<Customer, Integer>{
-
+	
+	/**
+	 * Search By name
+	 * @param name name of customer
+	 * @return
+	 */
+	public List<Customer> findByName(String name);
+	
+	/**
+	 * Search by lastName
+	 * @param lastName LastName of customer
+	 * @return
+	 */
+	public List<Customer> findByLastName(String lastName);
+	
+	/**
+	 * Find by dni
+	 * @param dni
+	 * @return
+	 */
+	public Customer findByDni(String dni);
 }
