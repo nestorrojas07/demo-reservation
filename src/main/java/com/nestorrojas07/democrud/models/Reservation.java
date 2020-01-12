1ba6a477d3f5f8b48b4a431e22f70fda19416afc
Nestor Rojas <nestor.rojas@zinobe.com>
2020-01-11 21:39:05 -0500

    Relations Ship map Customer and Reservation

--- src/main/java/com/nestorrojas07/democrud/models/Reservation.java
+++ src/main/java/com/nestorrojas07/democrud/models/Reservation.java
@@ -1,15 +1,14 @@
-/**
- * 
- */
 package com.nestorrojas07.democrud.models;
 
-import java.util.Set;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
@@ -22,23 +21,25 @@ import lombok.Data;
  */
 @Data
 @Entity
-@Table(name = "customers")
-public class Customer {
+@Table(name = "reservations")
+public class Reservation {
 	@Id
 	@Column(name = "id")
 	private Long id;
 	@GeneratedValue(generator = "uuid2")
 	@GenericGenerator(name = "uuid2", strategy = "uuid2")
 	private String uuid;
-	private String dni;
-	private String name;
-	private String lastName;
-	private String email;
-	private String address;
-	private String phoneNumber;
 	
-	//relationShip
-	@OneToMany(mappedBy = "customer")
-	private Set<Reservation> reservations;
+	private Date from_at;
+	private Date to_at;
 	
+	private Date created_at;
+	//relations ship
+	@ManyToOne
+	@JoinColumn(name = "customer_id")
+	private Customer customer;
+	
+	
+	
+
 }
