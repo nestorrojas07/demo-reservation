633ebcf7cd323b9daeda4b0c692731b1f4c2a55e
Nestor Rojas <nestor.rojas@zinobe.com>
2020-01-14 07:05:10 -0500

    Dedine Controller Resource

---
+++ src/main/java/com/nestorrojas07/democrud/controllers/CustomerController.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.nestorrojas07.democrud.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Clase Controladora que va a representar los servicios Rest
+ * @author nrojas
+ *
+ */
+@RestController
+@RequestMapping("/api/customer")    //mapea el nombre del api
+public class CustomerController {
+
+}
