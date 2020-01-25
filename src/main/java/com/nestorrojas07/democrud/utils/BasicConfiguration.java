e52a11db427085655d1b59c78f9e661f27e2b5dc
Nestor Rojas <nestor.rojas@zinobe.com>
2020-01-25 10:44:49 -0500

    Confgiracion del flujo de la session

--- src/main/java/com/nestorrojas07/democrud/utils/BasicConfiguration.java
+++ src/main/java/com/nestorrojas07/democrud/utils/BasicConfiguration.java
@@ -1,10 +1,15 @@
 package com.nestorrojas07.democrud.utils;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import ch.qos.logback.core.encoder.Encoder;
 
 /**
  * Clase para manejar los temas de seguridad
@@ -17,6 +22,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 
+	/**
+	 * Modificar el método configurar la página de login, error 
+	 * y satisfacción las cuales se enlaza con el controlador Login.
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
@@ -27,6 +36,30 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 		.failureUrl("/app/login?error=true")
 		.defaultSuccessUrl("/app/home");
 	}
+	
+	/**
+	 * Configurar el método de autentificación, para lo cual utilizamos un método en memoria.
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		builder.inMemoryAuthentication()
+		.withUser("nestor")
+		.password(encoder.encode("clave"))
+		.roles("USER");
+	}
+	
+	/**
+	 * Configurar qué rutas de la aplicación se deben excluir del proceso de login, 
+	 * en la cual están todos los recursos estáticos configurados anteriormente.
+	 */
+	@Override
+	public void configure(WebSecurity webSecurity) throws Exception {
+		webSecurity.ignoring()
+        .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+	}
+	
+	
 
 	
 
